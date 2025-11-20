0c9bf6d10b132bd7b1ce466f8bbf648f61227a52
Frederick2-2 <RunFrederick2@gmail.com>
2025-11-20 11:48:48 -0800

    stuff...

---
+++ PestControl/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*
+ * Frederick Southworth
+ * 11/20/2025
+ * This script shall control the player, how it acts, moves, and interacts with things within the game
+ */
+
+public class PlayerController : MonoBehaviour
+{
+    public int playerSpeed = 10;
+    public float playerJump = 7;
+    Vector3 playerDirection;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+}
