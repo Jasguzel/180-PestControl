62becb64b6d37b69a2e585571c5b2847ae30df22
Jasguzel <jasmineguzeldere@gmail.com>
2025-12-02 11:43:38 -0800

    item stuff

--- PestControl/Assets/Scripts/InventoryManager.cs
+++ PestControl/Assets/Scripts/InventoryManager.cs
@@ -2,6 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * Author: Guzeldere, Jasmine
+ * Last Updated: [12/02/2025]
+ * Handles the collecting and storing of items for the player using a list
+ */
 public class InventoryManager : MonoBehaviour
 {
     // Start is called before the first frame update
---
+++ PestControl/Assets/Scripts/Item.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+/*
+ * Author: Guzeldere, Jasmine
+ * Last Updated: 12/02/2025
+ * Storing the basic information of items
+ * 
+ */
+public class Item : MonoBehaviour
+{
+    public string itemsName = "";
+    public int doubloons = 0;
+    public TMP_Text nameText;
+    public TMP_Text costText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nameText.text = itemsName;
+        costText.text = "This Item Costs: " + doubloons + " doubloons!";
+    }
+}
