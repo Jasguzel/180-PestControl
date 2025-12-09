4e1063ccedaaa581767ecb5f86387ef25756fd99
Frederick2-2 <RunFrederick2@gmail.com>
2025-12-09 11:55:03 -0800

    spider behavior

--- PestControl/Assets/Scripts/SpiderScript.cs
+++ PestControl/Assets/Scripts/SpiderScript.cs
@@ -1,25 +1,134 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class SpiderScript : MonoBehaviour
 {
-    private int DetectionRange = 20;
+    public NavMeshAgent agent;
+    public Transform player;
+    public LayerMask whatIsGround, whatIsPlayer;
+    public float health;
 
+    //this will be the enemies patrolling function
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
 
+    //this will be enemy attacks
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+    public GameObject webs;
+
+    //different states the enemy is in
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //this will call all functions within enemy bahavior function
+        EnemyBehavior();
     }
-    private void playerDetection()
+    //functions other than start and update begin here
+    private void Awake()
     {
-
+        player = GameObject.Find("PlayerObj").transform;
+        agent = GetComponent<NavMeshAgent>();
     }
+    private void EnemyBehavior()
+    {
+        //this part will check sight and attack range
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        //these will determine what the spider does regarding player position
+        if(!playerInSightRange && !playerInAttackRange)
+        {
+            Patrolling();
+        }
+        if (playerInSightRange && !playerInAttackRange)
+        {
+            ChasePlayer();
+        }
+        if(playerInSightRange && playerInAttackRange)
+        {
+            AttackPlayer();
+        }
+
+        Patrolling();
+        ChasePlayer();
+        AttackPlayer();
+    }
+    private void Patrolling()
+    {
+        if (!walkPointSet)
+        {
+            SearchWalkPoint();
+        }
+        if (!walkPointSet)
+            agent.SetDestination(walkPoint);
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        //walkpoint reached
+        if (distanceToWalkPoint.magnitude < 1f)
+            walkPointSet = true;
+    }
+    private void SearchWalkPoint()
+    {
+        //this calculates the random point within range of the player
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+            walkPointSet = true;
+    }
+    private void ChasePlayer()
+    {
+        agent.SetDestination(player.position);
+    }
+    private void AttackPlayer()
+    {
+        agent.SetDestination(transform.position);
+
+        transform.LookAt(player);
+
+        if (alreadyAttacked)
+        {
+            //The ranged attack behavior code goes here
+            Rigidbody rb = Instantiate(webs, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
+            rb.AddForce(transform.forward * 12f, ForceMode.Impulse);
+            rb.AddForce(transform.up * 1f, ForceMode.Impulse);
+
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+    public void Damaged(int damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+        {
+            Invoke(nameof(DestroyEnemy), 0.5f);
+        }
+    }
+    private void DestroyEnemy()
+    {
+        Gizmos.color = Color.black;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, sightRange);    }
 }
