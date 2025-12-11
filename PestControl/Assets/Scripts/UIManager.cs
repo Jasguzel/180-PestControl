b25d52046b2227d3c9b50c5ab6d13a476ae0a4a9
Jasguzel <jasmineguzeldere@gmail.com>
2025-12-10 22:29:58 -0800

    Stuff

--- PestControl/Assets/Scripts/UIManager.cs
+++ PestControl/Assets/Scripts/UIManager.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-
+/*
+ * Guzeldere, Jasmine
+ * 12/10/2025
+ * Managing some UI Components
+ */
 public class UIManager : MonoBehaviour
 {
+    public PlayerController playerController;
+    public TMP_Text doubloonsText;
+    public TMP_Text scoreText;
+    public TMP_Text healthText;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,8 @@ public class UIManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        scoreText.text = "Score: " + playerController.totalScore;
+        doubloonsText.text = "Doubloons: " + playerController.doubloons;
+        healthText.text = "Health Remaining: ";
     }
 }
