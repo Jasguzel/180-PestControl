e694ffa83b1becc369622171aedb724ca2f6fe82
Jasguzel <jasmineguzeldere@gmail.com>
2025-12-10 19:56:44 -0800

    DOUBLoONS (monkeycelebration)

---
+++ PestControl/Assets/Scripts/Doubloons.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Guzeldere, Jasmine
+ * 12/10/2015
+ * Handles the behaviour/existance of coin objects
+ */
+public class Doubloons : MonoBehaviour
+{
+    public int doubloonsValue = 1;
+    public float doubloonRotateSpeed = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, doubloonRotateSpeed * Time.deltaTime * 10, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerController>())
+        {
+            other.GetComponent<PlayerController>().doubloons += doubloonsValue;
+            Destroy(gameObject);
+        }
+    }
+}
